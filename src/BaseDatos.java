8a3c5e667c2d3a6efa639136c18dcfcd2c5cca93
lcossp00 <lcossp00@estudiantes.unileon.es>
2019-08-28 09:43:42 +0200

    File xml creado. hibernate corregido

--- src/BaseDatos.java
+++ src/BaseDatos.java
@@ -1,4 +1,9 @@
 
+import org.hibernate.SessionFactory;
 public class BaseDatos {
-
+	//pringao
+	
+	public void insertarNovela(String tituloOriginal, String tituloMeta, String titulosIncluidos, String isbn) {
+		SessionFactory sesionFactoria = NewHibernateUtil.get
+	}
 }
